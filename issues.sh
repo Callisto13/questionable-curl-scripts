b2093065ab3d18a313b8ccc6311c4a16aca34c80
Claudia Beresford <claudiaberesford@gmail.com>
2021-04-27 11:01:39 +0100

    update for monthly data

--- issues.sh
+++ issues.sh
@@ -1,42 +1,61 @@
 #!/bin/bash
 
-BUG_DIR="/tmp/bug_counts"
-BUG_FILE=output.csv
+ISSUE_DIR="/tmp/bug_counts"
+HELP_OUT=helps.csv
+BUG_OUT=bugs.csv
+FEATURE_OUT=features.csv
 
-mkdir -p "$BUG_DIR"
-rm $BUG_FILE
+mkdir -p "$ISSUE_DIR"
+rm $HELP_OUT $BUG_OUT $FEATURE_OUT
 
-trap 'rm -rf $BUG_DIR' EXIT
+trap 'rm -rf $ISSUE_DIR' EXIT
 
-total=0
-ownerFound=0
-communityFound=0
-closed=0
+# january=0
+# february=0
+# march=0
 
-for i in {1..14}; do
-   curl \
-      -u "Callisto13:$GITHUB_TOKEN" \
+# comment this out to simply parse files again
+# TODO have a flag for this
+for i in {1..100}; do # have to go quite far back through years of issues
+   curl -u "Callisto13:$GITHUB_TOKEN" \
       -H "Accept: application/vnd.github.v3+json" \
-      "https://api.github.com/repos/weaveworks/eksctl/issues?state=all&page=$i&per_page=100" \
-      > "$BUG_DIR/page$i.json"
-
-   count=$(jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | length' < "$BUG_DIR/page$i.json" | wc -l)
-   total=$((total+count))
-
-   ownerFoundCount=$(jq -r '.[] |  select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | select(.user.login == "errordeveloper" or .user.login == "martina-if" or .user.login == "Callisto13" or .user.login == "aclevername" or .user.login == "cPu1" or .user.login == "michaelbeaumont") | length' < "$BUG_DIR/page$i.json"  | wc -l)
-   ownerFound=$((ownerFound+ownerFoundCount))
-
-   communityFoundCount=$(jq -r '.[] |  select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | select(.user.login != "errordeveloper" and .user.login != "martina-if" and .user.login != "Callisto13" and .user.login != "aclevername" and .user.login != "cPu1" and .user.login != "michaelbeaumont") | length' < "$BUG_DIR/page$i.json"  | wc -l)
-   communityFound=$((communityFound+communityFoundCount))
-
-   countClosed=$(jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | select(.state == "closed") | length' < "$BUG_DIR/page$i.json" | wc -l)
-   closed=$((closed+countClosed))
-
-   jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | [.number,.created_at,.title,.state,.closed_at] | @csv' < "$BUG_DIR/page$i.json" >> "$BUG_FILE"
-   # jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | [.number,.created_at,.title,.state,.closed_at,.author_association] | @csv' < "$BUG_DIR/page$i.json" >> "$BUG_FILE"
+      "https://api.github.com/repos/weaveworks/eksctl/issues?state=closed&page=$i&per_page=100" \
+      > "$ISSUE_DIR/page$i.json"
 done
 
-echo "\"total\",\"$total\""
-echo "\"ownerFound\",\"$ownerFound\""
-echo "\"communityFound\",\"$communityFound\""
-echo "\"ofWhichClosed\",\"$closed\""
+# TODO for number of files or smth
+for i in {1..100}; do
+   # curl \
+   #    -u "Callisto13:$GITHUB_TOKEN" \
+   #    -H "Accept: application/vnd.github.v3+json" \
+   #    "https://api.github.com/repos/weaveworks/eksctl/issues?state=closed&page=$i&per_page=100" \
+   #    > "$ISSUE_DIR/page$i.json"
+
+   # jcount=$(jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.closed_at | contains("2021-01")) | length' < "$ISSUE_DIR/page$i.json" | wc -l)
+   # january=$((january+jcount))
+   # fcount=$(jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.closed_at | contains("2021-02")) | length' < "$ISSUE_DIR/page$i.json" | wc -l)
+   # february=$((february+fcount))
+   # mcount=$(jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.closed_at | contains("2021-03")) | length' < "$ISSUE_DIR/page$i.json" | wc -l)
+   # march=$((march+mcount))
+
+   # ownerFoundCount=$(jq -r '.[] |  select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | select(.user.login == "errordeveloper" or .user.login == "martina-if" or .user.login == "Callisto13" or .user.login == "aclevername" or .user.login == "cPu1" or .user.login == "michaelbeaumont") | length' < "$ISSUE_DIR/page$i.json"  | wc -l)
+   # ownerFound=$((ownerFound+ownerFoundCount))
+
+   # communityFoundCount=$(jq -r '.[] |  select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | select(.user.login != "errordeveloper" and .user.login != "martina-if" and .user.login != "Callisto13" and .user.login != "aclevername" and .user.login != "cPu1" and .user.login != "michaelbeaumont") | length' < "$ISSUE_DIR/page$i.json"  | wc -l)
+   # communityFound=$((communityFound+communityFoundCount))
+
+   # countClosed=$(jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | select(.state == "closed") | length' < "$ISSUE_DIR/page$i.json" | wc -l)
+   # closed=$((closed+countClosed))
+
+   # TODO date var
+   jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.closed_at | contains("2021-03")) | [.number,.closed_at] | @csv' < "$ISSUE_DIR/page$i.json" >> "$BUG_OUT"
+   jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/help") | select(.closed_at | contains("2021-03")) | [.number,.closed_at] | @csv' < "$ISSUE_DIR/page$i.json" >> "$HELP_OUT"
+   jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/feature") | select(.closed_at | contains("2021-03")) | [.number,.closed_at] | @csv' < "$ISSUE_DIR/page$i.json" >> "$FEATURE_OUT"
+   # jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | [.number,.created_at,.title,.state,.closed_at] | @csv' < "$ISSUE_DIR/page$i.json" >> "$HELP_OUT"
+   # jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | [.number,.created_at,.title,.state,.closed_at,.author_association] | @csv' < "$ISSUE_DIR/page$i.json" >> "$HELP_OUT"
+done
+
+# echo "\"january\",\"$january\""
+# echo "\"february\",\"$february\""
+# echo "\"march\",\"$march\""
+# echo "\"ofWhichClosed\",\"$closed\""
--- releases.sh
+++ releases.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# get releases for the year. 100 is magic, but does cover the whole of 2020
+# get releases for the year. 100 is magic, but does cover the whole year
 # can be made smarter in future
+# also make date configurable
 curl \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/weaveworks/eksctl/releases?per_page=100 | \
-  jq '.[] | select(.published_at | contains("2020")) | select(.prerelease == false) | [.tag_name,.created_at] | @csv' -r
+  jq '.[] | select(.published_at | contains("2021-03")) | select(.prerelease == false) | [.tag_name,.created_at] | @csv' -r
 
