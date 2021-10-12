682b502e4d2ead68aefeb85f27406b7aa10c951c
Claudia Beresford <claudiaberesford@gmail.com>
2021-10-12 14:18:42 +0100

    improve issues script

--- issues.sh
+++ issues.sh
@@ -1,61 +1,100 @@
 #!/bin/bash
 
-ISSUE_DIR="/tmp/bug_counts"
-HELP_OUT=helps.csv
-BUG_OUT=bugs.csv
-FEATURE_OUT=features.csv
+ISSUE_DIR="/tmp/issues"
+YEAR=2021
+MONTH=09
+USERNAME=
+SKIP_CLEANUP=
 
-mkdir -p "$ISSUE_DIR"
-rm $HELP_OUT $BUG_OUT $FEATURE_OUT
+usage() {
+cat << EOF
+usage: $0 -y YEAR -m MONTH [-u USERNAME]
 
-trap 'rm -rf $ISSUE_DIR' EXIT
+Script to generate release data for eksctl and profiles projects.
 
-# january=0
-# february=0
-# march=0
+OPTIONS:
+   -y YEAR     Set the year (default: 2021)
+   -m MONTH    Set the month in digital (default: 10)
+   -u USERNAME Set the github username. Used in conjunction with the $TOKEN env var to avoid API rate limiting.
+   -s          Skip cleaning up and re-curling
+   -h          Show this message
+EOF
+}
 
-# comment this out to simply parse files again
-# TODO have a flag for this
-for i in {1..100}; do # have to go quite far back through years of issues
-   curl -u "Callisto13:$GITHUB_TOKEN" \
-      -H "Accept: application/vnd.github.v3+json" \
-      "https://api.github.com/repos/weaveworks/eksctl/issues?state=closed&page=$i&per_page=100" \
-      > "$ISSUE_DIR/page$i.json"
+curl_repo() {
+   echo "Fetching issue data for $1, saving to $ISSUE_DIR"
+   AUTH=
+   if [[ -z "$TOKEN" || -z "$USERNAME" ]]; then
+      echo "TOKEN variable not set. Recommend setting a github token and username to avoid API rate limiting. See usage -h for more."
+   else
+      AUTH="-u $USERNAME:$TOKEN"
+   fi
+
+   for i in $(seq 1 "$2"); do
+      curl -s \
+         "$AUTH" \
+         -H "Accept: application/vnd.github.v3+json" \
+         "https://api.github.com/repos/weaveworks/$1/issues?state=closed&page=$i&per_page=100" \
+         > "$ISSUE_DIR/page$i.json"
+   done
+}
+
+parse_issues() {
+   local out_dir="$1"
+   local help_out="$out_dir/helps.csv"
+   local bug_out="$out_dir/bugs.csv"
+   local feature_out="$out_dir/features.csv"
+   local zip_file="$YEAR-$MONTH-$1.zip"
+   rm "$help_out" "$feature_out" "$bug_out" || true
+
+   echo "Parsing issue data for $1, zipping to ./$zip_file"
+
+   files=$(find $ISSUE_DIR -type f | wc -l)
+   for i in $(seq 1 "$files"); do
+      jq -r ".[] | select(.pull_request == null) | select(.labels[].name == \"kind/bug\") | select(.closed_at | contains(\"$YEAR-$MONTH\")) | [.number,.closed_at] | @csv" < "$ISSUE_DIR/page$i.json" >> "$bug_out"
+      jq -r ".[] | select(.pull_request == null) | select(.labels[].name == \"kind/help\") | select(.closed_at | contains(\"$YEAR-$MONTH\")) | [.number,.closed_at] | @csv" < "$ISSUE_DIR/page$i.json" >> "$help_out"
+      jq -r ".[] | select(.pull_request == null) | select(.labels[].name == \"kind/feature\") | select(.closed_at | contains(\"$YEAR-$MONTH\")) | [.number,.closed_at] | @csv" < "$ISSUE_DIR/page$i.json" >> "$feature_out"
+   done
+
+   zip "$zip_file" "$1"
+   echo "Done."
+}
+
+while getopts ":hsu:y:m:" OPTION
+do
+  case $OPTION in
+    h)
+      usage
+      exit
+      ;;
+    y)
+      YEAR=$OPTARG
+      ;;
+    m)
+      MONTH=$OPTARG
+      ;;
+    u)
+      USERNAME=$OPTARG
+      ;;
+    s)
+      SKIP_CLEANUP=1
+      ;;
+    ?)
+      usage
+      exit
+      ;;
+  esac
 done
 
-# TODO for number of files or smth
-for i in {1..100}; do
-   # curl \
-   #    -u "Callisto13:$GITHUB_TOKEN" \
-   #    -H "Accept: application/vnd.github.v3+json" \
-   #    "https://api.github.com/repos/weaveworks/eksctl/issues?state=closed&page=$i&per_page=100" \
-   #    > "$ISSUE_DIR/page$i.json"
 
-   # jcount=$(jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.closed_at | contains("2021-01")) | length' < "$ISSUE_DIR/page$i.json" | wc -l)
-   # january=$((january+jcount))
-   # fcount=$(jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.closed_at | contains("2021-02")) | length' < "$ISSUE_DIR/page$i.json" | wc -l)
-   # february=$((february+fcount))
-   # mcount=$(jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.closed_at | contains("2021-03")) | length' < "$ISSUE_DIR/page$i.json" | wc -l)
-   # march=$((march+mcount))
+if [[ -z "$SKIP_CLEANUP" ]]; then
+   rm -rf "$ISSUE_DIR" && (mkdir -p "$ISSUE_DIR" || true)
 
-   # ownerFoundCount=$(jq -r '.[] |  select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | select(.user.login == "errordeveloper" or .user.login == "martina-if" or .user.login == "Callisto13" or .user.login == "aclevername" or .user.login == "cPu1" or .user.login == "michaelbeaumont") | length' < "$ISSUE_DIR/page$i.json"  | wc -l)
-   # ownerFound=$((ownerFound+ownerFoundCount))
+   curl_repo eksctl 100
+   curl_repo pctl 50
+   curl_repo profiles 50
+fi
 
-   # communityFoundCount=$(jq -r '.[] |  select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | select(.user.login != "errordeveloper" and .user.login != "martina-if" and .user.login != "Callisto13" and .user.login != "aclevername" and .user.login != "cPu1" and .user.login != "michaelbeaumont") | length' < "$ISSUE_DIR/page$i.json"  | wc -l)
-   # communityFound=$((communityFound+communityFoundCount))
-
-   # countClosed=$(jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | select(.state == "closed") | length' < "$ISSUE_DIR/page$i.json" | wc -l)
-   # closed=$((closed+countClosed))
-
-   # TODO date var
-   jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.closed_at | contains("2021-03")) | [.number,.closed_at] | @csv' < "$ISSUE_DIR/page$i.json" >> "$BUG_OUT"
-   jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/help") | select(.closed_at | contains("2021-03")) | [.number,.closed_at] | @csv' < "$ISSUE_DIR/page$i.json" >> "$HELP_OUT"
-   jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/feature") | select(.closed_at | contains("2021-03")) | [.number,.closed_at] | @csv' < "$ISSUE_DIR/page$i.json" >> "$FEATURE_OUT"
-   # jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | [.number,.created_at,.title,.state,.closed_at] | @csv' < "$ISSUE_DIR/page$i.json" >> "$HELP_OUT"
-   # jq -r '.[] | select(.pull_request == null) | select(.labels[].name == "kind/bug") | select(.created_at | contains("2020")) | [.number,.created_at,.title,.state,.closed_at,.author_association] | @csv' < "$ISSUE_DIR/page$i.json" >> "$HELP_OUT"
-done
-
-# echo "\"january\",\"$january\""
-# echo "\"february\",\"$february\""
-# echo "\"march\",\"$march\""
-# echo "\"ofWhichClosed\",\"$closed\""
+parse_issues eksctl
+parse_issues pctl
+parse_issues profiles
